6f4fb7bd66cf4b9396cf0328604d398273badfbd
RebeccaOgunyemi <olufunmilayosmiles@gmail.com>
2022-09-15 10:57:32 +0100

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
-
-"""
-A script that takes in an argument and displays all values
-in the states table of hbtn-0e_0_usa where name matches the argument
-and is safe from SQL Injection.
-"""
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    my_db = MySQLdb.connect(user=argv[1],
-                            passwd=argv[2],
-                            db=argv[3],
-                            host='localhost',
-                            port=3306)
-    c = my_db.cursor()
-    cmd = """SELECT cities.id, cities.name, states.name
-             FROM states
-             INNER JOIN cities ON states.id = cities.state_id
-             ORDER BY cities.id ASC;"""
-    c.execute(cmd)
-    cities = c.fetchall()
 
-    for city in cities:
-        print(city)
-
-    c.close()
-    my_db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
